9587bfbb42c3d073e35d48453e47f31f0f80992c
alessiaortile <alessiaortile@users.noreply.github.com>
2024-01-18 12:42:05 +0000

    Changes from ruff autoformatting

--- toxic_comments/predict_model.py
+++ toxic_comments/predict_model.py
@@ -7,6 +7,7 @@ import sys
 import pandas as pd
 import numpy as np
 
+
 # Token and Encode Function
 def tokenize_and_encode(tokenizer, comments):
     """Tokenize and encode comments using the BERT tokenizer.
@@ -31,10 +32,10 @@ def tokenize_and_encode(tokenizer, comments):
 
         # Tokenize and encode the comment using the BERT tokenizer
         encoded_dict = tokenizer.encode_plus(
-        comment,
-        # Add special tokens like [CLS] and [SEP]
-        add_special_tokens=True,
-        # Pad the comment to 'max_length' with zeros if needed
+            comment,
+            # Add special tokens like [CLS] and [SEP]
+            add_special_tokens=True,
+            # Pad the comment to 'max_length' with zeros if needed
             # Depricated but other does not seem to work..
             padding="longest",
             # Return attention mask to mask padded tokens
