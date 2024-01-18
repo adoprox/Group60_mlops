17f698ef5356e69615e53fbb273029286b967bff
Andreas Stehle <andreas.stehle2@gmail.com>
2024-01-18 15:14:40 +0100

    make streamlit niver

--- toxic_comments/api/streamlit_input_inference.py
+++ toxic_comments/api/streamlit_input_inference.py
@@ -3,7 +3,15 @@ from omegaconf.dictconfig import DictConfig
 import streamlit as st
 from toxic_comments.predict_model import predict, load_model
 from hydra import initialize, compose
+from datetime import datetime
 
+prediction_labels = ["toxic","severe_toxic","obscene","threat","insult","identity_hate"]
+
+st.title("Check if a comment is toxic")
+
+st.write(f'This little application uses a BERT model trained on the toxic comment dataset to check if the text in the text field is toxic or not.')
+
+st.write(f'It will predict the probability of the text being in each of the following classes: {prediction_labels}')
 
 @st.cache_resource
 def load_config():
@@ -31,5 +39,11 @@ input = [input]
 # def run_input_prediction():
 #     output =
 if st.button(label="Run inference"):
-    output = predict(input, tokenizer, model, device)
-    output
+    st.header("Results: ")
+    prediction = predict(input, tokenizer, model, device)
+    output_with_labels = zip(prediction_labels, prediction[0][0])
+    results_formatted = [f'* {label}: {p*100:.1f}%' for label, p in output_with_labels]
+
+    for line in results_formatted:
+        st.markdown(line)
+    st.write(f'Computed on {datetime.now().strftime("%H:%M:%S")}')
